36564136b3b3b5c0d9040ca8087b7ee17f322248
cocoparp <2356641@cegepoutaouais.qc.ca>
2024-04-07 12:26:02 -0400

    Commit labo interface

---
+++ Semaine 10/Exercice interface/classe_Etudiant.py
@@ -0,0 +1,97 @@
+from datetime import datetime
+
+class Etudiant:
+    """
+    Classe Étudiant
+    """
+    ls_etudiants = []
+    def __init__(self, p_num_etudiant: int = 0, p_nom: str = "", p_programme: str = "",
+                 p_date_naissance: datetime = None):
+        """
+        Constructeur de la classe Etudiant
+        :param p_num_etudiant: Le numéro étudiant de l'étudiant
+        :param p_nom: Le nom de l'étudiant
+        :param p_programme: Le programme de l'étudiant
+        :param p_date_naissance: La date de naissance de l'étudiant
+        """
+        self._num_etudiant = p_num_etudiant
+        self._nom = p_nom
+        self._programme = p_programme
+        self._date_naissance = p_date_naissance
+        Etudiant.ls_etudiants.append(self)
+
+    def _get_num_etudiant(self):
+        return self._num_etudiant
+
+    def _set_num_etudiant(self, v_num_etudiant: int):
+        if isinstance(v_num_etudiant, int) and len(str(v_num_etudiant)) == 7:
+            self._num_etudiant = v_num_etudiant
+
+    num_etudiant = property(_get_num_etudiant, _set_num_etudiant)
+
+    def _get_nom(self):
+        return self._nom
+
+    def _set_nom(self, v_nom: str):
+        if isinstance(v_nom, str) and len(v_nom) < 25:
+            self._nom = v_nom
+
+    nom = property(_get_nom, _set_nom)
+
+    def _get_programme(self):
+        return self._programme
+
+    def _set_programme(self, v_programme: str):
+        self._programme = v_programme
+
+    programme = property(_get_programme, _set_programme)
+
+    def _get_date_naissance(self):
+        return self._date_naissance
+
+    def _set_date_naissance(self, v_date_naissance):
+        self._date_naissance = v_date_naissance
+
+    def afficher_liste_etudiants(cls):
+        """
+        Affiche les étudiants instanciés dans la classe
+        """
+        for etudiant in cls.ls_etudiants:
+            print(etudiant)
+
+    def age(self):
+        """
+        Permet de calculer l'age de l'étudiant instancié
+        """
+        if self._date_naissance:
+            annee_actuelle = datetime.now().year
+            annee_naissance = self._date_naissance.year
+            age = annee_actuelle - annee_naissance
+            return age
+        else:
+            return None
+
+    @classmethod
+    def chercher_etudiant(cls, numero_etudiant):
+        """
+        Permet de chercher un étudiant dans la liste des étudiants par son numéro étudiant
+        :param numero_etudiant: Le numéro étudiant de l'étudiant
+        """
+        for etudiant in cls.ls_etudiants:
+            if etudiant._num_etudiant == numero_etudiant:
+                return etudiant
+        return None
+
+    def __str__(self):
+        """
+        Méthode magique qui affiche toutes les informations de l'étudiant
+        """
+        return (f"""
+*************************************
+
+Numéro étudiant: {self._num_etudiant}
+Nom: {self._nom}
+Programme: {self._programme}
+Age: {self.age()}
+
+*************************************""")
---
+++ Semaine 10/Exercice interface/main.py
@@ -0,0 +1,139 @@
+# importer le fichier du ui converti en py
+from PyQt5.QtCore import pyqtSlot
+
+import interface_gestion
+
+from classe_Etudiant import Etudiant
+
+#Importer le module sys nécessaire à l'exécution.
+import sys
+
+#Importer la librairie QtWidgets de QtDesigner.
+from PyQt5 import QtWidgets
+
+# créer une classe qui hérite de Qt et de notre ui.
+# Nom de ma classe (demoQt)         # Nom de mon fichier ui
+class demoQt(QtWidgets.QMainWindow, interface_gestion.Ui_MainWindow):
+    '''
+    Nome de la classe : demoQt
+    Héritages :
+    - QtWidgets.QMainWindow : Type d'interface créé par QtDesigner
+    - exercice_applicatif.Ui_MainWindow : Ma classe générée avec QtDesigner
+    '''
+    def __init__(self, parent=None):
+        '''
+        Constructeur de la classe
+        :param parent: QtWidgets.QMainWindow et interface_gestion.Ui_MainWindow
+        '''
+        # Appeler le constructeur parent avec ma classe en paramètre...
+        super(demoQt, self).__init__(parent)
+        self.setupUi(self) #Préparer l'interface utilisateur.
+
+    # Gérer les évènements
+    #   on _ nom de mon objet _ nom de l'évènement
+    @pyqtSlot()
+    def on_pushButton_Ajouter_clicked(self):
+        '''
+        Gestionnaire d'évènement pour le bouton Valider
+        '''
+        try:
+            num_etudiant = self.lineEdit_num_etudiant.text()
+            nom = self.lineEdit__nom_etudiant.text()
+            programme = self.comboBox.currentText()
+            date_naissance = self.dateEdit.date().toPyDate()
+            if num_etudiant.isdigit() and len(str(num_etudiant)) == 7 and isinstance(nom, str):
+                etudiant = Etudiant(num_etudiant, nom, programme, date_naissance)
+                Etudiant.ls_etudiants.append(etudiant)
+                self.textBrowser.append(str(etudiant))
+            else:
+                self.label_message_erreur.setText("Attention : valeurs entrées incorrectes !")
+                self.lineEdit_num_etudiant.clear()
+                self.lineEdit__nom_etudiant.clear()
+        except Exception as e:
+            print(f"Une erreur s'est produite : {e}")
+
+    @pyqtSlot()
+    def on_comboBox_changed(self):
+        """
+        Gestionnaire d'évènement pour le changement de sélection dans le combo box
+        """
+        try:
+            programme = self.comboBox.currentText()
+            print(programme)
+            if Etudiant.ls_etudiants:
+                etudiant = Etudiant.ls_etudiants[-1]
+                etudiant.programme = programme
+                print(etudiant)
+                self.textBrowser.append(str(etudiant))
+        except Exception as e:
+            print(f"Une erreur s'est produite: {e}")
+
+    @pyqtSlot()
+    def on_pushButton_Modifier_clicked(self):
+        """
+        Gestionnaire d'évènements si la bouton Modifier est cliqué
+        """
+        try:
+            num_etudiant = self.lineEdit_num_etudiant.text()
+            nouveau_nom = self.lineEdit__nom_etudiant.text()
+            nouveau_programme = self.comboBox.currentText()
+            etudiant = Etudiant.chercher_etudiant(num_etudiant)
+            if etudiant:
+                if nouveau_nom:
+                    etudiant.nom = nouveau_nom
+                if nouveau_programme:
+                    etudiant.programme = nouveau_programme
+                self.textBrowser.clear()
+                for etudiant in Etudiant.ls_etudiants:
+                    self.textBrowser.append(str(etudiant))
+                self.label_message_erreur.setText("Étudiant modifié avec succès !")
+            else:
+                self.label_message_erreur.setText("Aucun étudiant trouvé...")
+        except Exception as e:
+            print(f"Une erreur s'est produite: {e}")
+
+    @pyqtSlot()
+    def on_pushButton_Supprimer_clicked(self):
+        """
+        Gestionnaire d'évènements si le bouton Supprimer est cliqué
+        """
+        try:
+            num_etudiant = self.lineEdit_num_etudiant.text()
+            etudiant = Etudiant.chercher_etudiant(num_etudiant)
+            if etudiant:
+                Etudiant.ls_etudiants.remove(etudiant)
+                self.textBrowser.clear()
+                for etudiant in Etudiant.ls_etudiants:
+                    self.textBrowser.append(str(etudiant))
+                self.label_message_erreur.setText("Étudiant supprimé avec succès !")
+            else:
+                self.label_message_erreur.setText("Aucun étudiant trouvé...")
+        except Exception as e:
+            print(f"Une erreur s'est produite: {e}")
+
+    @pyqtSlot()
+    def on_pushButton_Sauvegarder_clicked(self):
+        """
+        Gestionnaire d'évènements si le bouton Sauvegarder est cliqué
+        """
+        try:
+            contenu_texte = self.textBrowser.toPlainText()
+            with open("liste_etudiants.txt", "w", encoding="utf-8") as f:
+                f.write(contenu_texte)
+            self.label_message_erreur.setText("Contenu sauvegardé avec succès !")
+        except Exception as e:
+            print(f"Une erreur s'est produite: {e}")
+
+# Créer le main qui lance la fenêtre de Qt
+def main():
+    '''
+    Méthode main : point d'entré du programme.
+    Exécution de l'applicatin avec l'interface graphique.
+    '''
+    app = QtWidgets.QApplication(sys.argv)
+    form = demoQt() #Nom de ma classe
+    form.show()
+    app.exec()
+
+if __name__ == "__main__":
+    main()
